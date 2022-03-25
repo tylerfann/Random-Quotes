33769fb3942f87f41fdc39b2357f3a960eb54cac
Tyler Fann <ty.fann95@gmail.com>
2022-03-24 21:55:40 -0400

    Added icon images for favorites and next/previous and handling state for hovering

--- components/Button.js
+++ components/Button.js
@@ -1,7 +1,12 @@
-const Button = ({ onClick, text, className, index }) => {
+import Image from 'next/image';
+
+
+const Button = ({ onMouseEnter, onMouseLeave, onClick, text, className, index, image, imageSize, imageStyle }) => {
   return (
-    <button disabled={index === 0} onClick={onClick} className={className}>
-      {text}
+    <button onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} disabled={index === 0} onClick={onClick} className={className}>
+      {image ? (
+        <Image className={imageStyle} src={image} width={imageSize.width} height={imageSize.height} />
+      ) : text}
     </button>
   );
 };
--- components/QuoteDisplay.js
+++ components/QuoteDisplay.js
@@ -1,4 +1,11 @@
+import { useState, useEffect } from "react";
 import Button from "./Button";
+import arrow from "../public/arrow.png";
+import arrowHoverIcon from "../public/arrow-hover.png";
+import heartThin from "../public/heart-thin.png";
+import heartThinHover from "../public/heart-thin-hover.png";
+import heartFull from "../public/heart-full.png";
+import heartFullHover from "../public/heart-full-hover.png";
 
 const QuoteDisplay = ({
   quote,
@@ -12,7 +19,38 @@ const QuoteDisplay = ({
   isLoading,
   unfavoriteOnclick,
 }) => {
+  const [heartHovered, setHeartHover] = useState(false);
+  const [nextArrowHovered, setNextArrowHover] = useState(false);
+  const [prevArrowHovered, setPrevArrowHover] = useState(false);
+
+  useEffect(() => {
+    setNextArrowHover(false);
+    setPrevArrowHover(false);
+  }, [id])
   const isFavorited = favorites[id] !== undefined;
+
+  const heartHover = (isHovered) => () => {
+    setHeartHover(isHovered);
+  };
+
+  let heartIcon;
+  if (heartHovered) {
+    heartIcon = isFavorited ? heartFullHover : heartThinHover;
+  } else {
+    heartIcon = isFavorited ? heartFull : heartThin;
+  }
+
+  const nextArrowHover = (isHovered) => () => {
+    setNextArrowHover(isHovered);
+  };
+
+  const prevArrowHover = (isHovered) => () => {
+    setPrevArrowHover(isHovered);
+  };
+
+  const nextArrowIcon = nextArrowHovered ? arrowHoverIcon : arrow;
+  const prevArrowIcon = prevArrowHovered ? arrowHoverIcon : arrow;
+
   return (
     <div className="flex justify-center">
       {isLoading ? (
@@ -26,6 +64,10 @@ const QuoteDisplay = ({
             text={isFavorited ? "un-favorite" : "favorite"}
             className={"self-end"}
             onClick={isFavorited ? unfavoriteOnclick : favoriteOnclick}
+            image={heartIcon}
+            imageSize={{ height: 32, width: 32 }}
+            onMouseEnter={heartHover(true)}
+            onMouseLeave={heartHover(false)}
           />
           <div className="flex justify-center p-3 m-3">
             <div className="w-3/4 flex flex-col">
@@ -36,13 +78,30 @@ const QuoteDisplay = ({
             </div>
           </div>
           <div className="flex justify-center">
+            {index === 0 ? (
+              <div className="p-1 m-1 w-8	h-8"></div>
+            ) : (
+              <Button
+                text="previous"
+                className={"p-1 m-1"}
+                imageStyle={"rotate-180"}
+                onClick={previousOnclick}
+                index={index}
+                image={prevArrowIcon}
+                imageSize={{ width: 32, height: 32 }}
+                onMouseEnter={prevArrowHover(true)}
+                onMouseLeave={prevArrowHover(false)}
+              />
+            )}
             <Button
-              text="previous"
+              image={nextArrowIcon}
+              imageSize={{ width: 32, height: 32 }}
+              text="next"
               className={"p-1 m-1"}
-              onClick={previousOnclick}
-              index={index}
+              onClick={nextOnclick}
+              onMouseEnter={nextArrowHover(true)}
+              onMouseLeave={nextArrowHover(false)}
             />
-            <Button text="next" className={"p-1 m-1"} onClick={nextOnclick} />
           </div>
         </div>
       )}
